9f273d2984d0c75040714270552f8fdb5bd24c59
fv-kalashikk <fv.kalashik@gmail.com>
2025-09-28 21:06:14 +0300

    Обновление README, ноутбука и загрузчика данных

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-file_id = "1S0IrXH7E3PW-EX-24lpV4xa0xC6kY5wu"  # ID файла на Google Drive
+file_id = "1fVkeUdzuBjqHsLPtL_AzUOeL4kX4Y7sV"  # ID файла на Google Drive
 file_url = f"https://drive.google.com/uc?id={file_id}"
 
 raw_data = pd.read_csv(file_url)  # читаем файл
